49a162c90f1276e16b3f4ec11b9bf93718da78ba
Kiraogi <golokron3@yandex.ru>
2025-01-18 20:33:50 +0300

    ver 0.12.01

--- parser_gui.py
+++ parser_gui.py
@@ -12,7 +12,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
-1
 
 # Глобальные переменные
 subcategories = {}
